2cac7d8eaa2c03a75b333fec1b7c7b9e6fca3721
Pedro Monteiro <pedro.msilva@dataprev.gov.br>
2025-03-23 14:17:57 -0300

    Mais imagens no carousel

--- src/paginas/Components/Carousel.jsx
+++ src/paginas/Components/Carousel.jsx
@@ -3,10 +3,14 @@ import img1 from '../../assets/landingPage/Finance Illustrations 1.png';
 import img2 from '../../assets/landingPage/Finance Illustrations 2.png';
 import img3 from '../../assets/landingPage/Finance Illustrations 3.png';
 import img4 from '../../assets/landingPage/Finance Illustrations 4.png';
+import img5 from '../../assets/landingPage/Finance Illustrations 5.png';
+import img6 from '../../assets/landingPage/Finance Illustrations 6.png';
+import img7 from '../../assets/landingPage/Finance Illustrations 7.png';
+import img8 from '../../assets/landingPage/Finance Illustrations 8.png';
 
 const Carrossel = () => {
     
-  const imagens = [img1, img2, img3, img4];
+  const imagens = [img1, img2, img3, img4, img5, img6, img7, img8];
 
   return (
     <Carousel interval={3000} controls={false} indicators={true} fade>
